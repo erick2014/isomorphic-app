ca73dbd8f101845ba6f29a1dcb00ea5e8f05b479
erick2014 <erickarmandogarcia@hotmail.com>
2015-11-15 20:26:26 -0500

    Implement the server file.

---
+++ server.js
@@ -0,0 +1,41 @@
+const express=require("express");
+const handleBars=require("express-handlebars");
+const http=require('http');
+const mongoose=require('mongoose');
+const twitter=require('twitter');
+const routes=require('./routes');
+const config=require('./config');
+const streamHandler=require('./utils/streamHandler');
+
+//create an express instance 
+var app=express();
+//set the port
+var port=process.env.PORT || 8080;
+
+//set handlebars as the templating engine
+app.engine('handlebars',handleBars({defaultLayout:'main'}));
+//set template engine to expres framework
+app.set('view engine','handlebars');
+
+//Disable etag headers on responses
+app.disable('etag');
+
+//connect to our mongo database 
+mongoose.connect('mongodb://localhost/react-tweets');
+
+//crete a new ntwitter instance
+var twit=new twitter(config.twitter);
+
+//root root route
+app.get('/',routes.index);
+
+//enable page route
+app.get('/page/:page/:skip',routes.page);
+
+//set /public as our static content dir
+app.use("/",express.static(__dirname+"/public/"));
+
+//fire our server up
+var server=http.createServer(app).listen(port,function(){
+	console.log("our server is running on port "+port)
+})
